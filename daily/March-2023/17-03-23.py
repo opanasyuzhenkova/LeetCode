997bd0b52e1a3bdc62200cb9824750a082aa7ace
Olya Panasyuzhenkova <panasyuzhenkova@gmail.com>
2023-03-17 12:02:11 +0300

    added task "208. Implement Trie (17-03-23.py)"

---
+++ daily/March-2023/17-03-23.py
@@ -0,0 +1,45 @@
+# 208. Implement Trie (Prefix Tree)
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_key = False # is this the last symbol of real word?
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_key = True
+        
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_key
+        
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+            
+
+
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple")) 
+print(trie.search("app"))
+print(trie.startsWith("app"))
+print(trie.insert("app"))
+print(trie.search("app"))  
