7dced882366778b0b65a859705ff1a23e1fa69cd
Olya Panasyuzhenkova <panasyuzhenkova@gmail.com>
2023-03-19 17:39:50 +0300

    added new tasks

--- daily/March-2023/19-03-23.py
+++ daily/March-2023/19-03-23.py
@@ -2,38 +2,37 @@
 
 # Trie Version
 
-# class TrieNode:
-#     def __init__(self):
-#         self.children = {}
-#         self.is_word = False
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
 
 
-# class WordDictionary:
-#     def __init__(self):
-#         self.root = TrieNode()
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
 
-#     def addWord(self, word: str) -> None:
-#         current_node = self.root
-#         for character in word:
-#             cur_node = cur_node.children.setdefault(character, TrieNode())
-#         cur_node.is_word = True
+    def addWord(self, word: str) -> None:
+        current_node = self.root
+        for character in word:
+            cur_node = cur_node.children.setdefault(character, TrieNode())
+        cur_node.is_word = True
 
-#     def search(self, word: str) -> bool:
-#         def dfs(node, index):
-#             if index == len(word):
-#                 return node.is_word
-#             if word[index] == ".":
-#                 for child in node.children.values():
-#                     if dfs(child, index + 1):
-#                         return True
-#             if word[index] in node.children:
-#                 return dfs(node.children[word[index]], index + 1)
-            
-#             return False
-        
-#         return dfs(self.root, 0)
+    def search(self, word: str) -> bool:
+        def dfs(node, index):
+            if index == len(word):
+                return node.is_word
+            if word[index] == ".":
+                for child in node.children.values():
+                    if dfs(child, index + 1):
+                        return True
+            if word[index] in node.children:
+                return dfs(node.children[word[index]], index + 1)
+            return False
+        return dfs(self.root, 0)
 
 
+# Defaultdict(list) Version
 
 from collections import defaultdict 
 
@@ -41,11 +40,9 @@ class WordDictionary:
         def __init__(self):
             self.words = defaultdict(list)
 
-
         def addWord(self, word: str) -> None:
             self.words[len(word)].append(word)
             print(self.words)
-
 
         def search(self, word: str) -> bool:
             n = len(word)
@@ -53,11 +50,10 @@ class WordDictionary:
                 for w in self.words[n]:
                     if all(word[i] in (w[i], '.') for i in range(n)):
                         return True
-                else: 
-                    return False
-
+                else: return False
             return word in self.words[n]
         
+
 obj = WordDictionary()
 obj.addWord('word')
 obj.addWord('.at')
