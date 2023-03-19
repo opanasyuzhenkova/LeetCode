29fe0347fbe03a56c87f8c07906bee84b38fbee5
Olya Panasyuzhenkova <panasyuzhenkova@gmail.com>
2023-03-19 17:34:24 +0300

    added new tasks

---
+++ easy/27-Remove-Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow 
+    
+print(Solution().removeElement([3, 2, 2, 3, 3], 3))
